91b640878b4dec903effa2031200ad619ed99323
Jon Leibham <jleibham@gmail.com>
2017-04-25 12:24:34 -0500

    changes

--- src/js/script.js
+++ src/js/script.js
@@ -1,10 +1,21 @@
 'use strict';
 import Registration from './components/registration';
+import Speaker from './components/speaker';
 
 function appInit() {
   // Create Registration Form
   if (document.querySelector(".form-container")) {
     const registration = new Registration();
+  }
+
+  // Create Speakers
+  if (document.querySelectorAll('.speaker-container')) {
+    const speakerContainers = document.querySelectorAll('.speaker-container');
+    for (let i = 0; i < speakerContainers.length; i++) {
+      if (speakerContainers[i].querySelector('.speaker-description') !== null) {
+        new Speaker(speakerContainers[i]);
+      }
+    }
   }
 }
 
